30a8c7064793f031f86b0c536cf9f883cccc9e64
naime <hossain.naime@yahoo.com>
2017-07-25 14:29:57 +0600

    405 error psge added for methodexception

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('heading')
+	{{-- expr --}}
+	<h1 class="text-center">opps!this page is not available</h1>
+
+ 
+@endsection
+@section('content')
+	<div class="col-md-8 col-md-offset-2">
+ <img src="/images/404.gif" class="img-responsive img-raised img-rounded" alt="">
+	
+
+</div>
+<div class="col-md-12 text-center">
+ 	<a href="/" class="btn btn-primary " title="">go home</a>
+ </div>
+
+
+	{{-- expr --}}
+@endsection
